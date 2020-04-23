ae037334e25f71b559f4e5013335f57a6b10b515
eric VENOT <eric.venot@inrae.fr>
2020-04-23 18:08:11 +0200

    Correction de la version 1.0 pour les Warnings et Note du Check Package

---
+++ R/globalvariables.R
@@ -0,0 +1,14 @@
+# ajout de toutes les variables des tables globales pour eviter le WARNING lors du check
+
+globalVariables(c("codepays",
+                  "colorRampPalette",
+                  "continent",
+                  "continentExp",
+                  "countriesAndTerritories",
+                  "countryterritoryCode",
+                  "dateRep",
+                  "deaths",
+                  "freq",
+                  "morts",
+                  "pays",
+                  "total_deces"))
